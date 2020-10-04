5703d367ba2640fb375e6e41a1c7660a43628fe1
statiqueplasma <h.aouadidrissi@gmail.com>
2020-10-04 17:07:12 +0100

    aded the show prifils feater

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ import os
 Window.size = (300,500)
 
 
+
+s_code = "E-123"
+u_code = "E-123"
+
 class CreateScreen(Screen):
     def add_user(self):
         conn = sqlite3.connect('icacard.db')
@@ -25,7 +29,7 @@ class CreateScreen(Screen):
         prenom = self.ids.prenom.text
         tel =self.ids.tel.text
         nbr=self.ids.nbr.text
-        print(nom,prenom,tel,nbr)   
+        #############################################################----------------------still need date------------------------#######################################################################
         cur.execute("INSERT INTO users VALUES (?,?,?,?,?)",(nom,prenom,'E-189',tel,nbr))
         conn.commit()
         conn.close()
@@ -41,16 +45,53 @@ class CreateStudent(Screen):
         nom = self.ids.stud_nom.text 
         prenom = self.ids.stud_prenom.text
         tel = self.ids.stud_tel.text
-        print(code_mem)
-        cur.execute("INSERT INTO students VALUES (?,?,?,?,?)",(nom,prenom,'E-200',tel,code_mem))
+        #############################################################----------------------still need date------------------------#######################################################################
+        cur.execute("INSERT INTO students VALUES (?,?,?,?,?)",(nom,prenom,'E-200',tel,u_code))
         conn.commit()
         conn.close()
         
-
 class ManageUsers(Screen):
     pass
 class Home(Screen):
     pass
+
+class StudentProfile(Screen):
+    def viewStudProfile(self):
+        conn = sqlite3.connect('icacard.db')
+        cur = conn.cursor()
+        #############################################################----------------------still need date------------------------#######################################################################     
+        cur.execute("SELECT nom,prénom,tel,code,code_affilié FROM students WHERE code=?",(s_code,))
+        stud = cur.fetchall()
+        self.ids.profil_s_nom.text = stud[0][0]
+        self.ids.profil_s_prenom.text = stud[0][1]
+        self.ids.profil_s_tel.text = str(stud[0][2])
+        self.ids.profil_s_code.text = stud[0][3]
+        self.ids.profil_s_par.text = stud[0][4]
+        u_code = stud[0][4]
+        cur.execute("SELECT nom,prénom FROM users WHERE code=?",(stud[0][4],))
+        users = cur.fetchall()
+        self.ids.profil_s_par_nom.text = users[0][0]
+        self.ids.profil_s_par_prenom.text = users[0][1]
+        conn.commit()
+        conn.close()
+        
+
+class UserProfile(Screen):
+    def viewUserProfile(self):
+        conn = sqlite3.connect('icacard.db')
+        cur = conn.cursor()
+        #############################################################----------------------still need date------------------------#######################################################################     
+        cur.execute("SELECT nom,prénom,tel,code,nombre_etudiants FROM users WHERE code=?",(u_code,))
+        users = cur.fetchall()
+        print(u_code)
+        print(users)
+        self.ids.profil_u_nom.text = users[0][0]
+        self.ids.profil_u_prenom.text = users[0][1]
+        self.ids.profil_u_tel.text = str(users[0][2])
+        self.ids.profil_u_code.text = u_code
+        self.ids.profil_u_nbr.text = str(users[0][4])
+        conn.commit()
+        conn.close()
 
 sm = ScreenManager()
 
@@ -60,6 +101,8 @@ sm.add_widget(ManageStudents(name='manage_students'))
 sm.add_widget(ManageUsers(name='manage_users'))
 sm.add_widget(Student_Code(name='student_code'))
 sm.add_widget(CreateStudent(name='create_stud'))
+sm.add_widget(StudentProfile(name='student_profile'))
+sm.add_widget(UserProfile(name='user_profile'))
 
 
 
@@ -120,7 +163,7 @@ class MainApp(MDApp):
 
         self.help_str.ids.screen_manager.get_screen('manage_users').children[0].add_widget(users_tables)
         self.help_str.ids.screen_manager.get_screen('manage_students').add_widget(students_tables)
-            
+
         
         return screen
     def user_check_press(self,instance_table,current_row):
@@ -149,15 +192,17 @@ class MainApp(MDApp):
         self.stud_code_tables.bind(on_row_press = self.row_press)
 
         self.help_str.ids.screen_manager.get_screen('student_code').ids.stud_code_title.text = code
-        global code_mem
-        code_mem = code
-        print(code_mem)
+        u_code = code
+
         self.help_str.ids.screen_manager.get_screen('student_code').ids.box_layout.add_widget(self.stud_code_tables)
         self.help_str.ids.screen_manager.current = "student_code"
         
         
     def check_press(self,instance_table,current_row):
-        pass
+        s_code = current_row[2]
+        self.help_str.ids.screen_manager.get_screen('student_profile').viewStudProfile()          
+        self.help_str.ids.screen_manager.current = "student_profile"
+
     def row_press(self,instance_table,current_row):
         pass
 
